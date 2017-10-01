ed762c6b8504e4bdff7d1755dc24e05f7fd28964
Yoshito Tabuchi @ MBA-Xamarin <ytabuchi.xlsoft@gmail.com>
2017-10-01 20:29:11 +0900

    Add Translate API

--- JavaKucheSample/JavaKucheSample/JavaKucheSamplePage.xaml.cs
+++ JavaKucheSample/JavaKucheSample/JavaKucheSamplePage.xaml.cs
@@ -16,27 +16,24 @@ namespace JavaKucheSample
 
         private async void PhotoButtonClicked(object sender, EventArgs e)
         {
-			// 未選択の場合はjaにします。
-			var lang = picker.SelectedItem == null ? "ja" : picker.SelectedItem.ToString();
+            // デフォルトのOCRの言語（＝翻訳のソース言語）はjaにします。
+            var sourceLang = picker.SelectedItem == null ? "ja" : picker.SelectedItem.ToString();
 
-			#region Get Photo Url
+            var targetLang = sourceLang == "ja" ? "en" : "ja";
 
-			var photoUrl = string.Empty;
+            #region Get Photo Url
+
+            var photoUrl = string.Empty;
             var imageChoiceResult = await DisplayAlert("画像の元を選択してください", "", "カメラ", "ギャラリー");
 
             try
             {
                 if (imageChoiceResult)
-                {
                     // 写真を撮影し、保存したURLを取得
                     photoUrl = await PhotoClient.TakePhotoAsync();
-                }
                 else
-                {
                     // Galleryに保存したURLを取得
                     photoUrl = await PhotoClient.PickPhotoAsync();
-
-                }
             }
             catch (Exception ex)
             {
@@ -56,25 +53,52 @@ namespace JavaKucheSample
 
             try
             {
-                indicatorLabel.Text = "Now doing OCR...";
+                indicatorLabel.Text = "Doing OCR now...";
                 indicatorFrame.IsVisible = true;
-                ocrResult = await OcrClient.DoOcrAsync(photoUrl, lang);
+
+                // Computer Vision APIからOcr結果をもらいLabelを更新します。
+                ocrResult = await OcrClient.DoOcrAsync(photoUrl, sourceLang);
                 ocredLabel.Text = $"OCR Result: {ocrResult}";
+
                 indicatorFrame.IsVisible = false;
                 indicatorLabel.Text = string.Empty;
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Error", ex.Message, "OK");
-				indicatorFrame.IsVisible = false;
-				indicatorLabel.Text = string.Empty;
+
+                indicatorFrame.IsVisible = false;
+                indicatorLabel.Text = string.Empty;
             }
+
+            if (string.IsNullOrEmpty(ocrResult))
+                return;
 
             #endregion
 
             #region Translate OCRed Text
 
             var translateResult = string.Empty;
+
+            try
+            {
+                indicatorLabel.Text = "Translating now...";
+                indicatorFrame.IsVisible = true;
+
+                // Translater Text APIから翻訳結果をもらいLabelを更新します。
+                translateResult = await TranslateClient.TranslateTextAsync(ocrResult, sourceLang, targetLang);
+                translatedLabel.Text = $"Translate Result: {translateResult}";
+
+                indicatorFrame.IsVisible = false;
+                indicatorLabel.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+
+                indicatorFrame.IsVisible = false;
+                indicatorLabel.Text = string.Empty;
+            }
 
             #endregion
 
--- JavaKucheSample/JavaKucheSample/TranslateClient.cs
+++ JavaKucheSample/JavaKucheSample/TranslateClient.cs
@@ -1,4 +1,4 @@
-﻿/* This class get a result from Translator API
+﻿/* This class get a result from Translator Text API
  * Tryout page:
  * http://docs.microsofttranslator.com/text-translate.html#!/default/get_Translate
  */
@@ -13,51 +13,50 @@ namespace JavaKucheSample
     {
         const string uriBase = @"https://api.microsofttranslator.com/v2/http.svc/Translate";
 
-        public static async Task<string> TranslateTextAsync(string rowText)
+        public static async Task<string> TranslateTextAsync(string rowText, string from, string to)
         {
             var token = await AuthenticationToken.GetBearerTokenAsync(Secrets.TranslatorTextApiKey);
 
             using (var client = new HttpClient())
             {
-                var sendUri = $"{uriBase}?appid={token}&text={rowText}&from=ja&to=en&category=generalnn";
+                var sendUri = $@"{uriBase}?appid={token}&text={rowText}&from={from}&to={to}&category=generalnn";
                 var stream = await client.GetStreamAsync(sendUri);
-                if (stream != null)
-                {
-                    var doc = XElement.Load(stream);
-                    return doc.Value;
-                }
-                return "couldn't translate.";
+                if (stream == null)
+                    throw new Exception("Could not translated.");
+
+                var doc = XElement.Load(stream);
+                return doc.Value;
             }
         }
     }
 
-	public class AuthenticationToken : IDisposable
-	{
-		private const string TokenUri = @"https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
-		private static readonly HttpClient Client;
+    public class AuthenticationToken : IDisposable
+    {
+        private const string tokenUri = @"https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+        private static readonly HttpClient client;
 
-		static AuthenticationToken()
-		{
-			Client = new HttpClient();
-		}
+        static AuthenticationToken()
+        {
+            client = new HttpClient();
+        }
 
-		public static async Task<string> GetBearerTokenAsync(string apiKey)
-		{
-			var token = await GetTokenAsync(apiKey);
-			//"Bearer "を付与してリターンしてます。
-			return $"Bearer {token}";
-		}
+        public static async Task<string> GetBearerTokenAsync(string apiKey)
+        {
+            var token = await GetTokenAsync(apiKey);
+            //"Bearer "を付与してリターンしてます。
+            return $"Bearer {token}";
+        }
 
-		private static async Task<string> GetTokenAsync(string apiKey)
-		{
-			Client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
-			var resuponse = await Client.PostAsync(TokenUri, new StringContent(string.Empty));
-			return await resuponse.Content.ReadAsStringAsync();
-		}
+        private static async Task<string> GetTokenAsync(string apiKey)
+        {
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+            var resuponse = await client.PostAsync(tokenUri, new StringContent(string.Empty));
+            return await resuponse.Content.ReadAsStringAsync();
+        }
 
-		public void Dispose()
-		{
-			//TODO Dispose support !
-		}
-	}
+        public void Dispose()
+        {
+            //TODO Dispose support !
+        }
+    }
 }
